f3c688593f28ddf6d610668c012988371146f3d3
refaee94 <m.refaee123580@gmail.com>
2022-06-19 15:03:06 +0400

    remove unused code

--- src/app/book/book-routing.module.ts
+++ src/app/book/book-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
-import { EditComponent } from './edit/edit.component';
 import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 
@@ -10,7 +9,6 @@ const routes: Routes = [
   { path: 'bookList/index', component: IndexComponent },
   { path: 'bookList/:bookListId/view', component: ViewComponent },
   { path: 'bookList/create', component: CreateComponent },
-  { path: 'bookList/:bookListId/edit', component: EditComponent }
 ];
 
 @NgModule({
